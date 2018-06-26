d1a2d192a794c45f33fd2e631ddc639dc298a570
Felix Mulder <felix.mulder@gmail.com>
2018-06-26 21:18:47 +0200

    Add working implementation of response / request generation

--- modules/swaggy/src/lambda/swaggy/definitions.scala
+++ modules/swaggy/src/lambda/swaggy/definitions.scala
@@ -1,7 +1,9 @@
 package lambda.swaggy
 
 import io.swagger.models._
+import io.swagger.models.properties._
 import scala.meta._
+
 import scala.collection.JavaConverters._
 
 object definitions {
@@ -13,25 +15,47 @@ object definitions {
         name + ".scala",
         pkg,
         List(
-          q"import io.circe.Decoder",
-          q"import io.circe.generic.semiauto.deriveDecoder",
+          q"import io.circe.{Decoder, Encoder}",
+          q"import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}",
+          q"import io.circe.java8.time._",
+          q"import java.time.Instant",
         ),
-        q"final case class $tpname()",
+        q"final case class $tpname(..${constructorParams(model)})",
         q"""
           object ${Term.Name(name)} {
             implicit val decoder: Decoder[$tpname] =
               deriveDecoder[$tpname]
+
+            implicit val encoder: Encoder[$tpname] =
+              deriveEncoder[$tpname]
           }
         """,
       )
     }
     .toList
 
+  private def constructorParams(m: Model): List[Term.Param] = m match {
+    case m: ModelImpl if m.getType == "object" =>
+      m.getProperties.asScala.map { case (name, tpe) =>
+        Term.Param(
+          Nil, Name(name), Some(propertyToType(tpe)), None
+        )
+      }
+      .toList
+    case _ => Nil
+  }
 
-  private def printModel(m: Model): Unit = m match {
-    case m: RefModel =>
-      println(m.getProperties)
-      println(m.getSimpleRef)
-      println(m.getExample)
+  private def propertyToType(prop: Property): Type = prop match {
+    case p: ArrayProperty => Type.Name(s"Array[${propertyToType(p.getItems)}]")
+    case _: BooleanProperty => Type.Name("Boolean")
+    case _: DoubleProperty => Type.Name("Double")
+    case p: DateProperty if p.getType == "string" => Type.Name("LocalDate")
+    case p: DateTimeProperty if p.getType == "string" => Type.Name("Instant")
+    case _: FloatProperty => Type.Name("Float")
+    case _: IntegerProperty => Type.Name("Int")
+    case _: LongProperty => Type.Name("Long")
+    case _: PasswordProperty => Type.Name("String")
+    case p: RefProperty => Type.Name(p.getSimpleRef)
+    case _: StringProperty => Type.Name("String")
   }
 }
