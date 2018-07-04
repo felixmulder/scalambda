d0fd6ef35abca89b3abb52be77843489c1252777
Felix Mulder <felix.mulder@klarna.com>
2018-07-04 16:29:46 +0200

    Add fst/snd helpers

--- modules/swaggy/src/lambda/swaggy/swaggy.scala
+++ modules/swaggy/src/lambda/swaggy/swaggy.scala
@@ -10,6 +10,9 @@ package object swaggy {
   type ErrorOr[A] = Validated[SwaggyError, A]
   type ErrorsOr[A] = ValidatedNel[SwaggyError, A]
 
-  val EmptyBody: Type.Name = Type.Name("EmptyBody")
+  private[swaggy] val EmptyBody: Type.Name = Type.Name("EmptyBody")
+
+  private[swaggy] def fst[A,B](t: (A, B)): A = t._1
+  private[swaggy] def snd[A,B](t: (A, B)): B = t._2
 
 }
