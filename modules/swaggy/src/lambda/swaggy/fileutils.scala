0e0a2166eb89d7166375d49baf1d13d0ac2dc2ac
Felix Mulder <felix.mulder@klarna.com>
2018-07-04 16:34:20 +0200

    Add read/write methods for trees

--- modules/swaggy/src/lambda/swaggy/fileutils.scala
+++ modules/swaggy/src/lambda/swaggy/fileutils.scala
@@ -9,13 +9,20 @@ import io.swagger.parser.SwaggerParser
 import cats.implicits._
 import cats.effect.IO
 import scala.meta._
+import org.scalafmt.Scalafmt.format
 
 object fileutils {
 
   def writeScalaFile(outputDir: String, file: ScalaFile): IO[Unit] =
-    IO.fromEither(ScalaFile.contents(file)).flatMap { contents =>
-      writeFile((outputDir :: file.pkg).toList.mkString("/"), file.title + ".scala", contents)
-    }
+    IO.fromEither(format(ScalaFile.toTree(file).syntax).toEither)
+      .map(_.getBytes)
+      .flatMap { bytes =>
+        writeFile(
+          (outputDir :: file.pkg).toList.mkString("/"),
+          file.title + ".scala",
+          bytes
+        )
+      }
 
   def writeScalaFiles(outputDir: String, files: List[ScalaFile]): IO[Unit] =
     files.map(writeScalaFile(outputDir, _)).sequence_
@@ -28,6 +35,25 @@ object fileutils {
         Files.write(path, contents)
       }
     )
+
+  def writeTrees(outputDir: String, filePathAndTree: List[(String, Tree)]): IO[Unit] =
+    filePathAndTree
+      .map { case (filePath, tree) =>
+        writeFile(outputDir, filePath, tree.syntax.getBytes)
+      }
+      .sequence_
+
+  def pathFrom(dir: String, f: ScalaFile): String =
+    dir + f.pkg.toList.mkString("/", "/", s"/${f.title}.scala")
+
+  def readFile(path: String): IO[Option[String]] =
+    IO(new String(Files.readAllBytes(Paths.get(path)))).attempt.map(_.toOption)
+
+  def readExisting[E <: ScalaFile](dir: String, f: E): IO[(E, Option[String])] =
+    readFile(pathFrom(dir, f)).map((f, _))
+
+  def readExisting[E <: ScalaFile](dir: String, fs: List[E]): IO[List[(E, Option[String])]] =
+    fs.map(readExisting(dir, _)).sequence
 
   def readSwagger(path: String): IO[Swagger] =
     IO(new SwaggerParser().read(path))
