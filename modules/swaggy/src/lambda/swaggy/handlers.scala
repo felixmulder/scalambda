61aba830af1a495fde783e90556d8c90909c365d
Felix Mulder <felix.mulder@klarna.com>
2018-07-04 16:32:10 +0200

    Return trees in ScalaFile.toTree

--- modules/swaggy/src/lambda/swaggy/handlers.scala
+++ modules/swaggy/src/lambda/swaggy/handlers.scala
@@ -3,12 +3,13 @@ package lambda.swaggy
 import cats.data.{NonEmptyList => NEL}
 import cats.implicits._
 
+
 object handlers {
   def apply(endpoints: List[Endpoint], pkg: NEL[String]): List[JsonHandler] =
     endpoints.flatMap(flattenOut(_, pkg))
 
   private[swaggy] def flattenOut(e: Endpoint, pkg: NEL[String]) =
     e.handlers.map { handler =>
-      JsonHandler(e.title, pkg, handler)
+      JsonHandler(e.title + handler.name.capitalize, e.title, pkg, handler)
     }
 }
