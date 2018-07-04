4bd9605c899fb1cb51d8d3f8f716c49aa1e42d3e
Felix Mulder <felix.mulder@klarna.com>
2018-07-04 16:34:42 +0200

    Add basic ability to only add missing handler functions to enpoints

--- modules/swaggy/src/lambda/swaggy/endpoints.scala
+++ modules/swaggy/src/lambda/swaggy/endpoints.scala
@@ -5,6 +5,7 @@ import io.swagger.models.HttpMethod._
 import io.swagger.models.parameters.{BodyParameter, QueryParameter}
 import scala.collection.JavaConverters._
 import scala.meta._
+import scala.meta.contrib._
 import cats.implicits._
 import cats.data.{Validated, NonEmptyList => NEL}
 
@@ -30,6 +31,76 @@ object endpoints {
     swagger.getPaths.asScala.foldLeft(Map.empty[String, ErrorsOr[Endpoint]])(addEndpoint)
       .values.toList.sequence
   }
+
+  def adaptExisting(existing: List[(Endpoint, Option[String])]): ErrorsOr[List[(String, Tree)]] = {
+    val shouldAdapt = existing.collect { case (e, Some(s)) => (e, s) }
+    val oldTs = parseExisting(shouldAdapt.map(snd))
+      .andThen { trees =>
+        shouldAdapt
+          .map(fst)
+          .zip(trees)
+          .map(adaptTree.tupled)
+          .sequence
+      }
+
+    val newTs = existing
+      .filter(snd(_).isEmpty)
+      .map(fst)
+      .map(ep => (ep, ScalaFile.toTree(ep)))
+
+    oldTs.map(_ ++ newTs).map(fstToFileName)
+  }
+
+  private[swaggy] def fstToFileName[A](xs: List[(Endpoint, A)]): List[(String, A)] =
+    xs.map {
+      case (k, v) => (s"${k.pkg.toList.mkString("/")}/${k.title}.scala", v)
+    }
+
+  private[swaggy] val adaptTree: (Endpoint, Tree) => ErrorsOr[(Endpoint, Tree)] =
+    (ep, tree) => {
+      def adaptedStats(xs: List[Stat]): List[Stat] = {
+        val handlers =
+          ep.handlers.groupBy(_.name)
+            .mapValues(_.map(ScalaFile.endpointMethod))
+
+        val defs = xs.collect { case d: Defn.Def => d }
+        val other = xs.filterNot(_.isInstanceOf[Defn.Def])
+
+        val defMap = defs.map(_.name.value).toSet
+        val nonExistingHandlers = handlers.filterKeys(!defMap.contains(_))
+
+        defs ++ nonExistingHandlers.flatMap(snd) ++ other
+      }
+
+      val obj = tree.find {
+        case obj: Defn.Object if obj.name.value == ep.title => true
+        case _ => false
+      }
+
+      if (obj.isDefined)
+        (ep -> tree.transform {
+          case obj: Defn.Object if obj.name.value == ep.title =>
+            obj.copy(
+              templ = obj.templ.copy(
+                stats = adaptedStats(obj.templ.stats)
+              )
+            )
+        })
+        .validNel
+        .map {
+          // This is needed until: https://github.com/scalameta/scalameta/pull/1527
+          case (ep, Source(trees)) => (ep, Source(trees))
+          case (ep, tree) => (ep, tree)
+        }
+      else MissingObject(ep.title).invalidNel
+    }
+
+  /** Parse fileBodies into trees */
+  private[swaggy] def parseExisting(existingMap: List[String]): ErrorsOr[List[Tree]] =
+    existingMap
+      .map(_.parse[Source].toEither.toValidatedNel)
+      .sequence
+      .leftMap(_.map(e => ParseError(e)))
 
   private[swaggy] def getOps(path: Path, pathString: String): ErrorsOr[List[MethodHandler]] =
     path
