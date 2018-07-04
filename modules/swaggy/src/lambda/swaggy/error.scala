9c6f3549130a1825e0a225012ea835347ca4aeaf
Felix Mulder <felix.mulder@klarna.com>
2018-07-04 16:34:42 +0200

    Add basic ability to only add missing handler functions to enpoints

--- modules/swaggy/src/lambda/swaggy/error.scala
+++ modules/swaggy/src/lambda/swaggy/error.scala
@@ -2,6 +2,7 @@ package lambda.swaggy.error
 
 import cats.data.NonEmptyList
 import io.swagger.models.HttpMethod
+import scala.meta.Parsed
 
 sealed abstract class SwaggyError(msg: String)
   extends RuntimeException(msg)
@@ -9,9 +10,14 @@ sealed abstract class SwaggyError(msg: String)
 final case class MultipleErrors(errors: NonEmptyList[SwaggyError])
   extends SwaggyError(errors.map(_.getMessage).toList.mkString("\n"))
 
+final case class MissingObject(title: String)
+  extends SwaggyError(s"Missing object definition in endpoint: $title.scala")
+
 final case class MissingBody(path: String, method: HttpMethod)
   extends SwaggyError(s"Missing body for $method in $path")
 
 final case class UnknownType(tpe: String, format: Option[String])
   extends SwaggyError(s"Unknown type: $tpe, with format: $format")
 
+final case class ParseError(err: Parsed.Error)
+  extends SwaggyError(err.toString)
