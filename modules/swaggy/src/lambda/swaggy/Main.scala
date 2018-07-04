a89d13a23cf20544392f14e91a10cf9e6ba70b9b
Felix Mulder <felix.mulder@klarna.com>
2018-07-04 16:46:59 +0200

    Adapt main to write correct trees to handler files

--- modules/swaggy/src/lambda/swaggy/Main.scala
+++ modules/swaggy/src/lambda/swaggy/Main.scala
@@ -6,17 +6,20 @@ import cats.effect.{IO, ExitCode, IOApp}
 
 import fileutils._
 import error._
+import syntax._
 
 object Main extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     for {
-      parsed  <- IO.fromEither(parseArgs(args))
-      swagger <- readSwagger(parsed.inputPath)
-      defs    <- IO(definitions(swagger, parsed.pkg ++ List("models")))
-      ends    <- endpoints(swagger, parsed.pkg ++ List("endpoints"))
-                   .fold(e => IO.raiseError(MultipleErrors(e)), IO.pure(_))
-      hands   <- IO(handlers(ends, parsed.pkg ++ List("handlers")))
-      _       <- writeScalaFiles(parsed.outputDir, defs ++ ends ++ hands)
+      parsed   <- IO.fromEither(parseArgs(args))
+      swagger  <- readSwagger(parsed.inputPath)
+      defs     <- IO(definitions(swagger, parsed.pkg ++ List("models")))
+      newEnds  <- endpoints(swagger, parsed.pkg ++ List("endpoints")).orRaiseMultiple[IO]
+      hands    <- IO(handlers(newEnds, parsed.pkg ++ List("handlers")))
+      existing <- readExisting(parsed.outputDir, newEnds.toList)
+      ends     <- endpoints.adaptExisting(existing).orRaiseMultiple[IO]
+      _        <- writeScalaFiles(parsed.outputDir, defs ++ hands)
+      _        <- writeTrees(parsed.outputDir, ends)
     } yield ExitCode.Success
 
   private final case class ParsedArgs(
